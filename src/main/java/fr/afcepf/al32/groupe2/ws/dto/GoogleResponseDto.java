aafd2137f6c5ff7832bd38422a1b98b8d4083f5d
dimi1926 <diniko1926@gmail.com>
2018-11-23 10:13:50 +0100

    sos

---
+++ src/main/java/fr/afcepf/al32/groupe2/ws/dto/GoogleResponseDto.java
@@ -0,0 +1,31 @@
+package fr.afcepf.al32.groupe2.ws.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GoogleResponseDto {
+
+	private List<Rows> rows;
+
+	private String status;
+
+	public List<Rows> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<Rows> rows) {
+		this.rows = rows;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
---
+++ src/main/java/fr/afcepf/al32/groupe2/ws/dto/TextValue.java
@@ -0,0 +1,17 @@
+package fr.afcepf.al32.groupe2.ws.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TextValue {
+	private Integer value;
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+}
