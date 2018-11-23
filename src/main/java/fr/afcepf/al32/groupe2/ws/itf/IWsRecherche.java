2f2c7c3fddae9c23d07d95245fb16e98d8fe2e83
Ramya CANABADY <jeyacumar.ramya@gmail.com>
2018-11-23 16:54:44 +0100

    update IWsRecherche

--- src/main/java/fr/afcepf/al32/groupe2/ws/itf/IWsRecherche.java
+++ src/main/java/fr/afcepf/al32/groupe2/ws/itf/IWsRecherche.java
@@ -1,5 +1,13 @@
 package fr.afcepf.al32.groupe2.ws.itf;
 
+import java.util.List;
+
+import fr.afcepf.al32.groupe2.ws.dto.CategoryProductDto;
+import fr.afcepf.al32.groupe2.ws.dto.PromotionDto;
+
 public interface IWsRecherche {
 
+	public List<PromotionDto> searchListPromotion
+	(String source,Integer perimetre,List<String> keywords, CategoryProductDto categoryProductDto);
+	
 }
