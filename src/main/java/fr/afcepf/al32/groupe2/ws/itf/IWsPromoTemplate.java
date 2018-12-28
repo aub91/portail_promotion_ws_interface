62668b4ed55b332d2e4f9a23524426acd9cd94a0
dimi1926 <diniko1926@gmail.com>
2018-12-28 11:06:19 +0100

    ptit ajout de la deuxieme methode

--- src/main/java/fr/afcepf/al32/groupe2/ws/itf/IWsPromoTemplate.java
+++ src/main/java/fr/afcepf/al32/groupe2/ws/itf/IWsPromoTemplate.java
@@ -1,9 +1,10 @@
 package fr.afcepf.al32.groupe2.ws.itf;
 
-import fr.afcepf.al32.groupe2.ws.wsPromoTemplate.dto.TopPromotionTemplateResultDto;
-
 import java.util.List;
+
+import fr.afcepf.al32.groupe2.ws.wsPromoTemplate.dto.TopPromotionTemplateResultDto;
 
 public interface IWsPromoTemplate {
     TopPromotionTemplateResultDto searchTopTemplatePromoForShopKeeper(Double sourceLong, Double sourceLat, List<String> categories);
+    TopPromotionTemplateResultDto searchByClientsFavoriteCategory(Double sourceLong, Double sourceLat, String category);
 }
